8d05909729214c86d6fb8b7b69c6f817da7669a3
Manisha <it20609030@my.sliit.lk>
2022-03-15 20:53:47 +0530

    Add Admin Dashboard

---
+++ app/src/main/java/com/example/kdy_2022_wd01/AdminDashboard.java
@@ -0,0 +1,14 @@
+package com.example.kdy_2022_wd01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_dashboard);
+    }
+}
